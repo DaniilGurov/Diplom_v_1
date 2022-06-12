76e4a094f53d401c6f64a0efca0cb3c75987b371
gurovvaleriy <info@it.domeo.ru>
2022-06-12 17:14:49 +0300

    AddCommentPartner

---
+++ MainApp/static/js/slider.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('.slider_index').slick({
+        arrows: true,
+        slidesToShow: 3,
+        adaptiveHeight: true,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 1000,
+        speed: 1000,
+    });
+});
